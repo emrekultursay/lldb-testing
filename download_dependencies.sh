aa627011745fc59b659f2fb042b5539ce0bf70ce
Emre Kultursay <emrekultursay@google.com>
2025-09-20 16:36:31 -0700

    Fix extracted ndk directory

--- download_dependencies.sh
+++ download_dependencies.sh
@@ -8,8 +8,8 @@ unzip cmake-3.22.1-linux.zip -d cmake/3.22.1/
 rm cmake-3.22.1-linux.zip
 
 wget https://dl.google.com/android/repository/android-ndk-r28c-linux.zip
-mkdir -p ndk/28.2.13676358
-unzip android-ndk-r28c-linux.zip -d ndk/28.2.13676358
+mkdir -p ndk/
+unzip android-ndk-r28c-linux.zip -d ndk/
 rm android-ndk-r28c-linux.zip
 
 
