b8b6f9fc07a84c43b4f6cd81f09778e6097d8ce2
Emre Kultursay <emrekultursay@google.com>
2025-09-20 17:02:43 -0700

    Move build steps from yml file to ci.sh script

---
+++ build_lldb_server.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo "Building lldb-server"
+
+set -x
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Cross compiling
+CMAKE="${SCRIPT_DIR}/cmake/3.22.1/bin/cmake"
+NINJA="${SCRIPT_DIR}/cmake/3.22.1/bin/ninja"
+ANDROID_NDK_HOME="${SCRIPT_DIR}/ndk/android-ndk-r28c"
+
+ANDROID_ABI=${ANDROID_ABI:-arm64-v8a}
+ANDROID_PLATFORM=android-24
+
+if [[ "$ANDROID_ABI" == "arm64-v8a" ]]; then
+  LLVM_HOST_TRIPLE=aarch64-unknown-linux-android
+elif [[ "$ANDROID_ABI" == "armeabi-v7a" ]]; then
+  LLVM_HOST_TRIPLE=arm-unknown-linux-androideabi
+else
+  echo "Invalid ANDROID_ABI=$ANDROID_ABI"
+  exit 1
+fi
+
+
+CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Debug}"
+
+BUILD_DIR="${SCRIPT_DIR}/build-${ANDROID_ABI}"
+OUT_DIR="${BUILD_DIR}/out"
+mkdir -p "${BUILD_DIR}"
+mkdir -p "${OUT_DIR}"
+
+pushd "${BUILD_DIR}"
+$CMAKE ../llvm-project/llvm -G Ninja \
+  -B "${OUT_DIR}" \
+  -DCMAKE_MAKE_PROGRAM="${NINJA}" \
+  -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
+  -DLLVM_ENABLE_PROJECTS="clang;lld;lldb" \
+  -DLLDB_ENABLE_PYTHON=0 \
+  -DLLDB_ENABLE_LIBEDIT=0 \
+  -DLLDB_ENABLE_CURSES=0 \
+  -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake" \
+  -DANDROID_ABI="${ANDROID_ABI}" \
+  -DANDROID_PLATFORM="${ANDROID_PLATFORM}" \
+  -DANDROID_ALLOW_UNDEFINED_SYMBOLS=On \
+  -DLLVM_HOST_TRIPLE="${LLVM_HOST_TRIPLE}" \
+  -DCROSS_TOOLCHAIN_FLAGS_NATIVE='-DCMAKE_C_COMPILER=cc;-DCMAKE_CXX_COMPILER=c++'
+
+pushd "${OUT_DIR}"
+time "${NINJA}" lldb-server
+
+echo "Stripping lldb-server binary to reduce size"
+"${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-strip bin/lldb-server"
+
---
+++ ci.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+set -ex
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+"${SCRIPT_DIR}/download_dependencies.sh"
+
+
+# TODO: enable these
+#ANDROID_ABI=arm64-v8a "${SCRIPT_DIR}/build_lldb_server.sh"
+#ANDROID_ABI=armeabi-v7a "${SCRIPT_DIR}/build_lldb_server.sh"
